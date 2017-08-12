7ca9d21fcd19f98470c482dc1cc70569b99ae1d7
Jorge Murta <jmurta.jm@gmail.com>
2017-08-12 11:52:34 +0100

    Changes to Default NUXT Layout

---
+++ components/NavBar.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <nav class="navbar ">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="http://jorgemurta.com">
+        Jorge Murta
+      </a>
+
+      <a class="navbar-item is-hidden-desktop" href="https://github.com/jgthms/bulma" target="_blank">
+        <span class="icon" style="color: #333;">
+          <i class="fa fa-github"></i>
+        </span>
+      </a>
+
+      <a class="navbar-item is-hidden-desktop" href="https://twitter.com/jgthms" target="_blank">
+        <span class="icon" style="color: #55acee;">
+          <i class="fa fa-twitter"></i>
+        </span>
+      </a>
+
+      <div class="navbar-burger burger" data-target="navMenuExample">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div id="navMenuExample" class="navbar-menu">
+      <div class="navbar-start">
+      </div>
+
+      <div class="navbar-end">
+      </div>
+    </div>
+  </nav>
+</template>
